4e08a75ed27012e3c0947f8662e98fa646c0524e
Merryn <merrynhr@gmail.com>
2021-06-21 14:36:12 +0100

    sign up page with devise

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -1,5 +1,6 @@
 class TweeetsController < ApplicationController
   before_action :set_tweeet, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /tweeets or /tweeets.json
   def index
@@ -26,7 +27,7 @@ class TweeetsController < ApplicationController
 
     respond_to do |format|
       if @tweeet.save
-        format.html { redirect_to @tweeet, notice: "Tweeet was successfully created." }
+        format.html { redirect_to root_path, notice: "Tweeet was successfully created." }
         format.json { render :show, status: :created, location: @tweeet }
       else
         format.html { render :new, status: :unprocessable_entity }
